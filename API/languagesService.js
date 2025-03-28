42b8e53d1e45ea756435803a3cb8c89d1b09fa37
JeremyVillodas <luisjeremy_19@hotmail.com>
2025-03-28 12:46:24 -0500

    feat: :sparkles: Add POST Student

--- API/languagesService.js
+++ API/languagesService.js
@@ -1,5 +1,22 @@
-var baseURL = " http://26.184.59.112:8000/api/";
+var baseURL = "http://26.26.10.104:8000/api/";
 var select = document.getElementById("language");
+var selectedLanguages = [];
+
+async function initializeLanguages() {
+  await insertLanguages();
+
+  var tagSelector = new MultiSelectTag("language", {
+    maxSelection: 5, // default unlimited.
+    required: true, // default false.
+    placeholder: "Ingresa un lenguaje", // default 'Search'.
+    onChange: function (selected) {
+      // Callback when selection changes.
+      console.log("Selection changed:", selected);
+      selectedLanguages = selected.map((item) => parseInt(item.id));
+    },
+  });
+}
+initializeLanguages();
 
 async function getLanguages(endpoint) {
   try {
--- API/studentService.js
+++ API/studentService.js
@@ -1,6 +1,11 @@
 var baseURL = " http://26.26.10.104:8000/api/";
 
 var rows = document.getElementById("tbody");
+
+var form = document.getElementById("form-student");
+
+var select = document.getElementById("language");
+
 async function getStudents() {
   try {
     const response = await fetch(baseURL + "students");
@@ -14,16 +19,50 @@ async function getStudents() {
 async function insertStudents() {
   try {
     var students = await getStudents();
-
     console.log(students);
-
+    rows.innerHTML = "";
     students.forEach((element) => {
-      rows.innerHTML += `<tr><td>${element.name}</td><td>${element.email}</td><td>${element.phone}</td><td>${element.name}</td><td><button>Editar</button><button>Eliminar</button></td></tr>`;
+      rows.innerHTML += `<tr><td>${element.name}</td><td>${element.email}</td><td>${element.phone}</td><td>Ingles</td><td><button>Editar</button><button>Eliminar</button></td></tr>`;
     });
   } catch (error) {
     console.log(error);
   }
 }
+
+async function postStudents(student) {
+  try {
+    const response = await fetch(baseURL + "students", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: student.name,
+        email: student.email,
+        phone: student.phone,
+        languages: student.language,
+      }),
+    });
+    console.log(response);
+    console.log(response.json.ok);
+    return await response.json;
+  } catch (error) {}
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const student = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    language: selectedLanguages,
+  };
+
+  console.log(student);
+  await postStudents(student);
+  insertStudents();
+});
 
 insertStudents();
 console.log(getStudents());
